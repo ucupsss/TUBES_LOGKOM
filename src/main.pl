c22269d250ac0a314cfc5bf3d4f6d0afed6d20ae
ucupsss <yusuffaishalals@gmail.com>
2025-05-20 15:52:07 +0700

    add : add main feature

--- src/main.pl
+++ src/main.pl
@@ -1,10 +1,18 @@
 % ==== main.pl ====
-:- consult('pokemon_game.pl').
-:- consult('pokemon.pl').
-:- consult('map.pl').
-:- consult('player.pl').
-:- consult('battle.pl').
-:- consult('utils.pl').
+:- include('pokemon.pl').
+:- include('map.pl').
+:- include('tas.pl').
 
 % Entry point
-:- initialization(startGame).
+startGame :-
+    write('--------Selamat datang di game POKEMONNNN---------'),nl,
+    spawnPlayer,
+    write('Masukkan username anda : '),
+    read(Username),
+    write('Halo '),write(Username), write(', Selamat bermain game'),nl,
+    tampilkanPokemonCommon.
+
+tampilkanPokemonCommon :-
+    write('Daftar Pokemon yang tersedia (common) :'), nl,
+    forall(pokemon(Nama, common, _, _, _, _, _, _, _, _),
+           (write('- '), write(Nama), nl)).
